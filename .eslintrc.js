0984b6e7069e00f4886e5bc415cb99c94395ddd8
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2023-02-20 21:57:32 +0000

    chore: change eslintrc to json

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:solid/typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'solid'],
-};
