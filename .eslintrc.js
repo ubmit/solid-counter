ba5bc2cb297581ba94643b416b184a0a1005a253
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2023-02-20 21:53:56 +0000

    chore: remove unnecessary options from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,9 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:solid/typescript',
   ],
-  overrides: [],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint', 'solid'],
-  rules: {},
 };
